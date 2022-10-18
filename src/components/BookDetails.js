54fadc043636a0a00e16164b461cefd49db20ce3
Lucile Jones <jones.lucile@gmail.com>
2022-10-18 11:03:47 -0600

    updated css

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -5,9 +5,9 @@ function BookDetails() {
     const { oneBook } = useContext(DataListContext)
 
     return (
-        <div>
-            <h3>{oneBook.title}</h3>
-            <p>{oneBook.description}</p>
+        <div className="book-details-container">
+            <h3 className="book-details-title">{oneBook.title}</h3>
+            <p className="book-details-description">{oneBook.description}</p>
         </div>
     )
 }
